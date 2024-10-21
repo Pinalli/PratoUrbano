22d288b7056ea3cf9fa56758110e445941c6ad8c
pinalli <betopinali@gmail.com>
2024-10-21 09:37:15 -0300

    feat: configuração de ambiente Docker para o projeto PratoUrbano com o Eureka
    - Adicionado arquivo docker-compose.yml com serviços para MySQL, Eureka Server e aplicação PratoUrbano.
    - Configurada healthcheck para os serviços MySQL e Eureka.
    - Configurado o Eureka Server com as propriedades básicas (porta, self-preservation desabilitado, etc).
    - Integrada a aplicação PratoUrbano com o Eureka para registro automático.
    - Configurada conexão com o banco de dados MySQL e mapeamento das portas.
    - Implementado monitoramento de saúde através do Actuator na aplicação PratoUrbano.
    - Validação do setup e comunicação entre os containers concluída com sucesso.

--- src/main/java/br/com/pinalli/PratoUrbanoApplication.java
+++ src/main/java/br/com/pinalli/PratoUrbanoApplication.java
@@ -3,13 +3,15 @@ package br.com.pinalli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "br.com.pinalli.repository")
-public class PratoUrbano {
+@EnableDiscoveryClient
+public class PratoUrbanoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PratoUrbano.class, args);
+        SpringApplication.run(PratoUrbanoApplication.class, args);
     }
 }
