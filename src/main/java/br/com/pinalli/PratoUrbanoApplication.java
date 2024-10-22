62cf207938d0a2c017efb1490f9e166cff143ad9
pinalli <betopinali@gmail.com>
2024-10-22 15:10:19 -0300

    fix: corrige configuração de rede Docker e conexão com Eureka
    Alterações realizadas:
    - Adicionada configuração explícita de rede para serviços MySQL e App
    - Corrigida conectividade de rede entre containers e Eureka Server
    - Adicionada especificação do driver bridge para pratourbano-network
    - Garantida resolução adequada do host.docker.internal com extra_hosts
    - Configuradas variáveis de ambiente para cliente Eureka
    
    Obs: Servidor Eureka deve estar rodando antes de iniciar os containers.

--- src/main/java/br/com/pinalli/PratoUrbanoApplication.java
+++ src/main/java/br/com/pinalli/PratoUrbanoApplication.java
@@ -3,12 +3,12 @@ package br.com.pinalli;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "br.com.pinalli.repository")
-@EnableDiscoveryClient
+
 public class PratoUrbanoApplication {
 
     public static void main(String[] args) {
